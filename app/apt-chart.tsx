7486876b01a7af1aaf8dfa134a9ae1ce02214d18
dhkim9549 <dhkim95@hotmail.com>
2024-09-21 01:13:04 +0000

    make mobile friendly

--- app/apt-chart.tsx
+++ app/apt-chart.tsx
@@ -1,8 +1,10 @@
-import { useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import bb, { scatter } from "billboard.js";
 import "billboard.js/dist/billboard.css";  // default css
 
 export default function AptChart({setChartRef}) {
+
+  let [sH, setSH] = useState(999);
 
   let chartRef = useRef();
 
@@ -58,18 +60,29 @@ export default function AptChart({setChartRef}) {
     chartRef.current = chart2;
     setChartRef(chart2);
 
+    setSH(window.innerHeight);
+
     window.addEventListener('resize', function(event) {
+      setSH(window.innerHeight);
       chartRef.current.resize({
-        width: Math.min(700, Math.min(window.screenWidth, window.innerWidth) - 50),
-        height: 450
+        width: Math.min(700, Math.min(window.screen.width, window.innerWidth) - 50),
+        height: Math.min(window.innerHeight * 0.93, 450)
       });
     }, true);
 
   }, []);
 
   return (
-    <div className="pt-20 px-5 flex flex-col bg-white">
-      <div id="chart2"></div>
+    <div>
+    {sH <= 400 ?
+      <div className="flex flex-col items-center bg-white fixed z-50 top-0 left-0 w-screen h-screen">
+        <div id="chart2"></div>
+      </div>
+    :
+      <div className="pt-20 px-5 flex flex-col bg-white">
+        <div id="chart2"></div>
+      </div>
+    }
     </div>
   )
 }

cca16292d077efcc08cae8bb7af1ddd117123e96
dhkim9549 <dhkim95@hotmail.com>
2024-10-04 08:45:10 +0000

    format maxCtrtDy

--- app/prod-item.tsx
+++ app/prod-item.tsx
@@ -3,11 +3,15 @@ import Button from '@mui/material/Button';
 
 export default function ProdItem({prodObj, addAptList}) {
 
+  function formatDate(d) {
+    return d.substring(0, 4) + '.' + d.substring(4, 6) + '.' + d.substring(6, 8);
+  }
+
   return(
     <Paper className="w-full lg:w-72 bg-white p-4 flex flex-col">
       <div className="text-black text-xl mb-1">{prodObj.aptNm}</div>
       <div className="text-slate-800">{prodObj.sggu}</div>
-      <div className="text-slate-800 text-sm">{prodObj.cnstYr}년 준공, 최종거래일: {prodObj.maxCtrtDy}</div>
+      <div className="text-slate-800 text-sm">{prodObj.cnstYr}년 준공, 최종거래일: {formatDate(prodObj.maxCtrtDy)}</div>
       <div className="mt-2 flex flex-wrap gap-2">
         {prodObj.areas &&
           prodObj.areas.map(x =>
